fac7089c5da24886fbb1d0039ba817c8867551fa
GabrielPereiraTosi <gabrieltosi6@gmail.com>
2021-06-14 18:37:13 -0300

    Adicionando funçao de guardar log's e config's e organizando o código

--- src/Organizando.py
+++ src/Organizando.py
@@ -2,51 +2,55 @@ import os
 from datetime import datetime
 
 
-def write_config(diretorioA, diretorioB, regx):
+def write_config(diretorio_a, diretorio_b, regx, periodo):
+    # Armazena as configurações usadas em um .txt
     with open('config.txt', 'a') as arquivo:
-        arquivo.write("Origin Folder: {}\nDestination Folder: {}\nRegx: {}\n".format(diretorioA, diretorioB, regx))
+        arquivo.write("{}\n{}\n{}\n{}\n".format(diretorio_a, diretorio_b, regx, periodo))
 
 
-def write_log(arquivo, diretorioA, diretorioB):
+def write_log(arquivo, diretorio_a, diretorio_b):
+    # Armazena os dados de cada arquivo movimentado em um .txt
     with open('log.txt', 'a') as log_file:
         data_hora_atual = datetime.now()
         data_hora_atual_s = data_hora_atual.strftime('%d/%m/%Y %H:%M')
-        conteudo_log = "File name: {}; Origin folder: {}; Destination folder: {}; Moved time: {}\n".format(arquivo,
-                                                                                                           diretorioA,
-                                                                                                           diretorioB,
-                                                                                                           data_hora_atual_s)
+        conteudo_log = "Nome arquivo: {}; Diretorio origem: {}; Diretorio destinatario: {}; Movido em: {}\n".format(
+            arquivo,
+            diretorio_a,
+            diretorio_b,
+            data_hora_atual_s)
         log_file.write(conteudo_log)
 
 
 def vizualizar_dados():
-    retorno = '-----------------------------------CONFIG----------------------------------\n'
-    with open('config.txt', 'r') as config:
-        config_r = config.readlines()
-    with open('log.txt', 'r') as log:
-        log_r = log.readlines()
+    retorno = '----------------------------------LOG----------------------------------\n'
+    if os.path.isfile('log.txt'):
+        with open('log.txt', 'r') as log:
+            log_r = log.readlines()
 
-    for linhaC in config_r:
-        retorno += linhaC + '\n'
-    retorno += '----------------------------------LOG----------------------------------\n'
-
-    for linhaL in log_r:
-        retorno += linhaL + '\n'
-
+        for linha_l in log_r:
+            retorno += linha_l + '\n'
+    else:
+        retorno += 'Nenhum arquivo com o regx informado...'
     return retorno
 
 
-def organizar(diretorioA, diretorioB, regx):
-    arquivos = os.listdir(diretorioA)
+def organizar(diretorio_a, diretorio_b, regx):
+    arquivos = os.listdir(diretorio_a)
 
     for arquivo in arquivos:
-        if arquivo.find(regx) >= 0:
-            write_log(arquivo, diretorioA, diretorioB)
-            os.rename(os.path.join(diretorioA, arquivo), os.path.join(diretorioB, arquivo))
+        # Move o arquivo encontrado para o diretorio destinatario caso o regx informado seja encontrado
+        if arquivo.rfind(regx) >= 0:
+            write_log(arquivo, diretorio_a, diretorio_b)
+            os.rename(os.path.join(diretorio_a, arquivo), os.path.join(diretorio_b, arquivo))
 
 
-if __name__ == '__main__':
-    config_file = open('configFile.txt', 'r')
-    configuracoes = config_file.readlines()
-    organizar(os.path.join(configuracoes[0].rstrip('\n')), os.path.join(configuracoes[1].rstrip('\n')),
-              configuracoes[2])
-    config_file.close()
+def guarda_log_e_config():
+    # Armazena os arquivos log.txt e config.txt em um pasta target
+    target_path = os.path.realpath('target')
+    if os.path.isfile('log.txt'):
+        log_nome = 'log_{}_{}-{}.txt'.format(datetime.now().date(), datetime.now().hour, datetime.now().minute)
+        log_path = os.path.realpath('log.txt')
+        os.rename(log_path, os.path.join(target_path, log_nome))
+    config_nome = 'config_{}_{}-{}.txt'.format(datetime.now().date(), datetime.now().hour, datetime.now().minute)
+    config_path = os.path.realpath('config.txt')
+    os.rename(config_path, os.path.join(target_path, config_nome))
--- src/View.py
+++ src/View.py
@@ -5,52 +5,72 @@ import os
 
 import PySimpleGUI as sg
 
-from src.Organizando import organizar, vizualizar_dados, write_config
+from src.Organizando import organizar, vizualizar_dados, write_config, guarda_log_e_config
 
 
 class View:
 
     def __init__(self):
-        sg.theme('black')
+        #Criação da tela
+        sg.theme('Reddit')
         layout = [
-            [sg.Text('RegX', size=(13, 1)),
-             sg.Input(key='regx', size=(20, 1))],
-            [sg.Text('Origin Folder', size=(13, 1)),
-             sg.Input(key='origin', size=(35, 1))],
-            [sg.Text('Destination Folder', size=(13, 1)),
-             sg.Input(key='destination', size=(35, 1))],
-            [sg.Text('Period'), sg.Combo(values=list(range(100)), key='periodo', default_value=60, size=(3, 1))],
-            [sg.Output(size=(130, 35), key='output')],
-            [sg.Button('Run App')]
+            [sg.Text('RegX', size=(18, 1)),
+             sg.Input(key='regx', size=(25, 1))],
+            [sg.Text('Diretório Origem', size=(18, 1)),
+             sg.Input(key='origin', size=(50, 1))],
+            [sg.Text('Diretório Destinatário', size=(18, 1)),
+             sg.Input(key='destination', size=(50, 1))],
+            [sg.Text('Período'), sg.Combo(values=list(range(1, 121)), key='periodo', default_value=60, size=(4, 1))],
+            [sg.Output(size=(175, 30), key='output')],
+            [sg.Button('Run')]
         ]
-        self.janela = sg.Window('Organizer', layout)
+        self.janela = sg.Window('Organizador de Arquivos', layout)
 
     def iniciar(self):
         while True:
+            #Pegando todos os eventos e valores recebidos na tela até que a mesma seja fechada
             evento, valores = self.janela.read()
 
             if evento == sg.WIN_CLOSED:
-                break
-            if evento == 'Run App':
                 if os.path.isfile('config.txt'):
-                    print('Já esta sendo executado um processo de organização, '
-                          'caso deseje mudar a configuração finalize o processo atual.')
+                    guarda_log_e_config()
+                break
+            if evento == 'Run':
+                #Caso o programa já esteja com um processo rodando não será possivel criar outro
+                if os.path.isfile('config.txt'):
+                    sg.popup('Já esta sendo executado um processo de organização, '
+                             'caso deseje mudar a configuração finalize o processo atual.')
+                #Validando campos para que nao estejam vazios ou com diretórios incorretos
+                elif (not os.path.isdir(valores['origin'])) or valores['origin'] == '':
+                    sg.popup('Diretório Origem', 'É necessário que se utilize um diretório válido para a organização.')
+                elif (not os.path.isdir(valores['destination'])) or valores['destination'] == '':
+                    sg.popup('Diretório Destinatário', 'É necessário que se utilize um diretório válido para a organização.')
+                elif valores['regx'] == '':
+                    sg.popup('RegX', 'É necessário que se utilize um Regx válido.')
+                #Inicia o processo de organização
                 else:
-                    write_config(valores['origin'], valores['destination'], valores['regx'])
-                    threading.Thread(target=self.job_threading, args=(valores['origin'],
-                                                                      valores['destination'],
-                                                                      valores['regx'],
-                                                                      valores['periodo']), daemon=True).start()
+                    print('Iniciando o processo de organização...')
+                    write_config(valores['origin'], valores['destination'], valores['regx'], valores['periodo'])
+                    with open('config.txt', 'r') as config:
+                        configuracoes = config.readlines()
+                        diretorio_a = configuracoes[0].rstrip('\n')
+                        diretorio_b = configuracoes[1].rstrip('\n')
+                        regx = configuracoes[2].rstrip('\n')
+                        periodo = int(configuracoes[3].rstrip('\n'))
+                        threading.Thread(target=self.job_threading, args=(diretorio_a, diretorio_b, regx,
+                                                                          periodo), daemon=True).start()
 
-    def job_threading(self, diretorioA, diretorioB, regX, periodo):
-        schedule.every(periodo).seconds.do(self.job, diretorioA, diretorioB, regX)
+    def job_threading(self, diretorio_a, diretorio_b, regX, periodo):
+        #Configura um agendamento de tarefa em que cada periodo em segundos a tarefa seja executada
+        schedule.every(periodo).seconds.do(self.job, diretorio_a, diretorio_b, regX)
         while True:
             schedule.run_pending()
             time.sleep(1)
 
-    def job(self, diretorioA, diretorioB, regX):
+    def job(self, diretorio_a, diretorio_b, regx):
+        #Chama o metodo principal para a organização de arquivos
         self.janela.find_element('output').Update('')
-        organizar(diretorioA, diretorioB, regX)
+        organizar(diretorio_a, diretorio_b, regx)
         print(vizualizar_dados())
 
 
